aeeba0b7a9a36d9a65e6c1940a0181b58bbbca3a
Em Don <emma.k.donnelly@gmail.com>
2017-01-18 23:07:21 +0000

    Add files via upload

---
+++ 013.comparisons.js
@@ -0,0 +1,26 @@
+//So far we've learned about three data types:
+//- strings (e.g. "dogs go woof!")
+//-numbers (e.g. 4, 10)
+//-booleans (e.g. false, 5 > 4)
+//Now let's learn more about comparison operators.
+
+//List of comparison operators:
+//- > Greater than
+//- < Less than
+//- <= Less than or equal to
+//- >= Greater than or equal to
+//- === Equal to
+//- !== Not equal to
+
+//Try to use each of the operators above.
+//Choose the correct comparison operator to make each of the four statements print out true.
+
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+
+//Answers: true, true, true, true
