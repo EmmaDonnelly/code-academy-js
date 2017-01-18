3e5728ae31d5c95fa1ee500503117551e45d4f07
Em Don <emma.k.donnelly@gmail.com>
2017-01-18 22:07:51 +0000

    Update 5.error-does-not-compute.js

--- 5.error-does-not-compute.js
+++ 5.error-does-not-compute.js
@@ -1,10 +1,7 @@
-//There are some things you can't do in the console. Computers only speak
-//certain languages, like the one you've been using today: JavaScript!
-//If you use words that aren't in the JavaScript language,
-//it will get confused and give you an error.
+//There are some things you can't do in the console. Computers only speak certain languages, like the one you've been using today: JavaScript!
+//If you use words that aren't in the JavaScript language, it will get confused and give you an error.
 
-//Try to confuse the interpreter by using a word it doesn't know, like eggplant.
-//It will give you a ReferenceError.
+//Try to confuse the interpreter by using a word it doesn't know, like eggplant. It will give you a ReferenceError.
 
 platypus
 
