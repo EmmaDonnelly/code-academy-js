75a411ae5045bd3366e0f3058e8a309991e04da3
Em Don <emma.k.donnelly@gmail.com>
2017-01-18 22:02:41 +0000

    Add files via upload

---
+++ 7.what-am-i-learning.js
@@ -0,0 +1,16 @@
+//This is JavaScript (JS), a programming language. There are many languages,
+//but JS has many uses and is easy to learn.
+
+//What can we use JavaScript for?
+//- make websites respond to user interaction
+//- build apps and games (e.g. blackjack)
+//- access information on the Internet (e.g. find out the top trending
+//words on Twitter by topic)
+//- organize and present data (e.g. automate spreadsheet work; data visualization)
+
+//Press Save & Submit Code to see an example of how JavaScript can be interactive!
+
+confirm('This is an example of using JS to create some interaction on a website.
+Click OK to continue!');
+
+//This made a dialogue box pop up, and when OK was clicked "true" was returned
