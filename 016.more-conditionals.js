21bb9833f39503e17220a216eace264f906ccc90
Em Don <emma.k.donnelly@gmail.com>
2017-01-19 00:09:12 +0000

    Add files via upload

---
+++ 016.more-conditionals.js
@@ -0,0 +1,31 @@
+//Now let's practice using if/else statements. Do as much as you can by yourself, but if you need a reminder, click the "Stuck? Get a hint!" button below.
+
+//01. Write an if/else statement, just like we did in the last exercise. Here's what the outline of the code looked like:
+
+if (condition)
+{
+    // if condition is true
+    // do this code
+}
+else // "otherwise"
+{
+    // do this code instead
+}
+
+//02. If your condition is true, use console.log to print "The condition is true".
+//03. Otherwise (else) when it is false, use console.log to print "The condition is false".
+//04. Make sure your condition evaluates to false, so that your program prints out "The condition is false".
+
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+
+if (10 < 5)
+{
+    console.log("The condition is true")
+}
+else // "otherwise"
+{
+    console.log("The condition is false")
+}
+
+//answer: Thsi comdition is false.
