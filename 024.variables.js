6f8d762151ddeb53de91e267449ac5fa8b8014bf
Em Don <emma.k.donnelly@gmail.com>
2017-01-20 23:43:40 +0000

    Add files via upload

---
+++ 024.variables.js
@@ -0,0 +1,21 @@
+//We have learned how to do a few things now: make strings, find the length of strings, find what character is in the nth position, do basic math. Not bad for a day's work!
+//To do more complex coding, we need a way to 'save' the values from our coding.
+//We do this by defining a variable with a specific, case-sensitive name.
+//Once you create (or declare) a variable as having a particular name, you can then call up that value by typing the variable name.
+
+//Code:
+//var varName = data;
+
+//Example:
+//a. var myName = "Leng";
+//b. var myAge = 30;
+//c. var isOdd = true;
+
+//Create a variable called myAge and type in your age.
+// To create a variable, we use only one equals sign
+// But to check if two values are equal, we use 3 equal signs.
+
+// declare your variable here:
+
+var myAge = 27;
+console.log(myAge);
