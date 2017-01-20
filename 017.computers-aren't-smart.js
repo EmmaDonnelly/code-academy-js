42a36a52a823c6275316b13c2f777ee98715ae28
Em Don <emma.k.donnelly@gmail.com>
2017-01-20 23:04:07 +0000

    Add files via upload

---
+++ 017.computers-aren't-smart.js
@@ -0,0 +1,18 @@
+//Well done! Now, computers are very literal. Syntax needs to be in exactly the right place for the computer to understand the code.
+//As you get started with programming, we will teach you many syntax rules. This is sort of like the grammar of programming languages. Grammar first, then programming poetry!
+
+//There are many mistakes in this code. Find them and fix them all.
+//You are doing what's called "debugging," a term popularized by Grace Hopper when she literally removed a moth from her computer.
+
+//// The computer doesn't worry about extra spaces between words or brackets
+// It just cares about the order of where things are placed
+// and that you have used the right characters (){}[]"";
+
+// For extra help, a program called a 'linter' is checking your code
+// and will put a red 'x' next to the first line that contains errors
+
+if (10 === 10) {
+    console.log("You got a true!");
+} else {
+    console.log("You got a false!");
+}
