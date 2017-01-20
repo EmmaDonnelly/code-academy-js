041f28b1eb7d7b1b1610f3287aac82a3323516d5
Em Don <emma.k.donnelly@gmail.com>
2017-01-20 23:04:13 +0000

    Add files via upload

---
+++ 019.math.js
@@ -0,0 +1,21 @@
+//We saw basic math before. The basic math symbols we learned in school work here.
+//Even the order in which the computer understands the math is the same as in school!
+
+//Code:
+//01. ( ): control order of operations
+//02. * and /: multiplication and division
+//03. - and +: subtraction and addition
+
+//Examples:
+//01. 100/10 evaluates to 10
+//02. "Jane".length + 5 evaluates to 9
+//03. 5*(3+1) evaluates to 20
+
+//01. Complete the missing bits of code to construct the if / else statement. Make the condition evaluate to true.
+//02. Finish the else statement by printing out the string "Error Error Error" to the console.
+
+if ("Jon".length * 2 / (2+1) === 2 ) {
+   console.log("The answer makes sense!");
+} else {
+   console.log("Error Error Error")
+}
