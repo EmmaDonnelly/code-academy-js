1dd2b39a17b44ef268c8aaeb834099c27ae00ef3
Em Don <emma.k.donnelly@gmail.com>
2017-01-21 01:08:14 +0000

    Add files via upload

---
+++ 2.functions/001.introduction.js
@@ -0,0 +1,16 @@
+//Programming is simply a way to give instructions to the computer.
+//In Getting Started, we learned about if / else statements.
+//We want to keep learning ways to instruct the computer to perform repeatable tasks efficiently.
+
+//Let's briefly review! Use an if / else to check how fast you're driving.
+//If speed is greater than 80, use console.log to print "Slow down"
+//Otherwise (else), use console.log to print "Drive safe"
+
+var speed = 65;
+
+if (speed > 80) {
+	console.log("Slow down");
+}
+else {
+	console.log("Drive safe");
+}
