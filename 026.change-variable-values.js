147dfd9d68996a88f74e28b3f974f3cc1284cd87
Em Don <emma.k.donnelly@gmail.com>
2017-01-21 00:00:55 +0000

    Add files via upload

---
+++ 026.change-variable-values.js
@@ -0,0 +1,23 @@
+//So far, we've seen
+//a. how to create a variable
+//b. how to use a variable
+
+//Let's now see how to change a variable's value. A variable's value is easily changed. Just pretend you are creating a new variable while using the same name of the existing variable!
+
+//Example:
+//var myAge = "Thirty";
+//Say I had a birthday and I want to change my age.
+//myAge = "Thirty-one";
+//Now the value of myAge is "Thirty-one"!
+
+//Follow the instructions on line 1, line 3, line 5 and line 8. We're using this method to show you the order in which you tell the computer what to do is very important.
+
+// On line 2, declare a variable myName and give it your name.
+var myName = "Emma";
+// On line 4, use console.log to print out the myName variable.
+console.log(myName);
+// On line 7, change the value of myName to be just the first 2
+// letters of your name.
+var myName = "Em";
+// On line 9, use console.log to print out the myName variable.
+console.log(myName);
